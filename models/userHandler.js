522dee8aa6d0ec3538a938500b58a6282e7a97ac
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-03-31 15:34:30 -0700

    finish for creating book

--- models/userHandler.js
+++ models/userHandler.js
@@ -3,10 +3,10 @@ const User = require("./user");
 
 async function userHandler(request, response) {
   const email = request.query.email;
-  console.log({email})
+  // console.log({email})
   await User.find({email: email}, function (err, items) {
       if (err) return console.error(err);
-      console.log(items[0].books);
+      // console.log(items[0].books);
       response.status(200).send(items[0].books);
   })
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 app.use(cors());
+app.use(express.json()); // middleware
 const PORT = process.env.PORT;
 
 const User = require("./models/user");
@@ -22,17 +23,39 @@ const elijah = new User({
     },
   ],
 });
-app.get("/books", userHandler);
 elijah.save();
 
 const ellis = new User({
   email: "dyioan@gmail.com",
   books: [
-   
+    { name: "Harry Potter", description: "Goblet of Fire", status: "read" },
+    { name: "Harry Potter", description: "Chamber of Secrets", status: "read" },
+    {
+      name: "Harry Potter",
+      description: "Half-Blood Prince",
+      status: "reading",
+    },
   ],
 });
-app.get("/books", userHandler);
 ellis.save();
+
+function createBook(req, res) {
+  // console.log("%c from createBook, ", req.body, "color: red");
+  const email = req.body.email;
+  const book = {name:req.body.bookName,description:req.body.description,status:req.body.status}
+  console.log(book)
+  User.findOne({email},(err,entry)=>{
+    if (err) return console.error(err);
+    entry.books.push(book);
+    entry.save();
+    console.log("after user",entry.books)
+    res.status(200).send(entry.books)
+  })
+}
+
+app.get("/books", userHandler);
+app.post("/books", createBook);
+// app.delete("/books",deleteBook);
 // console.log("elijahs books", elijah);
 
 //MiddleWares
@@ -42,7 +65,6 @@ ellis.save();
 
 //ROUTES
 app.get("/", (req, res) => {
-
   res.send("proof of life");
 });
 
